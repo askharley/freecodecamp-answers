522a1c1d8509a6a8348d84089c6d3d5e83152e45
Harley Ferguson <harl3y.fer@gmail.com>
2020-04-12 09:16:28 +0200

    Create finders-keepers.js

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/finders-keepers.js
@@ -0,0 +1,13 @@
+function findElement(arr, func) {
+  let num = undefined;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      num = arr[i];
+      break;
+    }
+  }
+  return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
