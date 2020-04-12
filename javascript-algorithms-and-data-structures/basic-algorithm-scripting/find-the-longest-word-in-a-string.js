4e2f9fc1592e41648f131e47965db5de9b7e3430
Harley Ferguson <harl3y.fer@gmail.com>
2020-04-12 08:53:41 +0200

    Create find-the-longest-word-in-a-string.js

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/find-the-longest-word-in-a-string.js
@@ -0,0 +1,18 @@
+function findLongestWordLength(str) {
+  let length = 0;
+  const arr = buildArray(str);
+  
+  arr.map(element => {
+    if (element.length > length) {
+      length = element.length;
+    }
+  });
+
+  return length;
+}
+
+function buildArray(str) {
+  return str.split(" ");
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
