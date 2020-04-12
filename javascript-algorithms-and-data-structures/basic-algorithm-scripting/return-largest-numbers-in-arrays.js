bbb61ef7f83c3895cac2f0efb03b099c9a060acf
Harley Ferguson <harl3y.fer@gmail.com>
2020-04-12 08:57:54 +0200

    Create return-largest-numbers-in-arrays.js

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/return-largest-numbers-in-arrays.js
@@ -0,0 +1,8 @@
+function largestOfFour(arr) {
+  const result = arr.map(currentArr => {
+    return Math.max(...currentArr);
+  });
+  return result;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
