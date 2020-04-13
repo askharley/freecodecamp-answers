906d04645f26caf4c8dd4212bc4a880e34331953
Harley Ferguson <harl3y.fer@gmail.com>
2020-04-13 09:27:16 +0200

    Create 18-inherit-behaviours-from-a-supertype.js

---
+++ javascript-algorithms-and-data-structures/object-orientated-programming/18-inherit-behaviours-from-a-supertype.js
@@ -0,0 +1,13 @@
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
