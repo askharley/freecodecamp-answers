8c8c964ea0394e582abfb869b43197c0c05d7f40
Harley Ferguson <harl3y.fer@gmail.com>
2020-04-13 09:48:40 +0200

    Create 26-use-a-iife-to-create-a-module.js

---
+++ javascript-algorithms-and-data-structures/object-orientated-programming/26-use-a-iife-to-create-a-module.js
@@ -0,0 +1,14 @@
+const funModule = (function () {
+  return {
+    isCuteMixin: function (obj) {
+      obj.isCute = function () {
+        return true;
+      };
+    },
+    singMixin: function (obj) {
+      obj.sing = function () {
+        console.log("Singing to an awesome tune");
+      };
+    }
+  }
+})();
